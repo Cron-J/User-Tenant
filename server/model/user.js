d16c783204e805d7a0e16bbb09323e49f636384d
unknown <anusha@cronj.com>
2015-04-15 12:33:03 +0530

    change in user model

--- server/model/user.js
+++ server/model/user.js
@@ -71,7 +71,7 @@ var User = new Schema({
     createdBy: {
         type: String,
         required: true,
-        enum: ['Admin', 'Tenant-Admin']
+        enum: ['Self Registraition', 'Admin', 'Tenant-Admin']
     },
 
     /**
@@ -80,7 +80,7 @@ var User = new Schema({
     updatedBy: {
         type: String,
         required: true,
-        enum: ['Admin', 'Tenant-Admin']
+        enum: ['Self Registraition', 'Admin', 'Tenant-Admin']
     },
 
     /**
