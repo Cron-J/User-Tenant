99178a75e02b25a487d8eea3e880892ef0fe4c3c
unknown <anusha@cronj.com>
2015-04-22 15:05:07 +0530

    added growl in maincontroller

--- client/src/app/controllers/mainCtrl.js
+++ client/src/app/controllers/mainCtrl.js
@@ -1,9 +1,9 @@
 'use strict';
 
 app.controller('mainCtrl', ['$scope', '$location', '$rootScope', '$http', '$modal',
-		'$log','userInfo', 'countryList', 'AuthServ',
+		'$log','userInfo', 'countryList', 'AuthServ', 'growl',
     function ($scope, $location, $rootScope, $http, $modal, $log, userInfo, 
-    	countryList, AuthServ) {
+    	countryList, AuthServ, growl) {
         var _scope = {};
         _scope.init = function () {
         	$scope.current_usr = {};
